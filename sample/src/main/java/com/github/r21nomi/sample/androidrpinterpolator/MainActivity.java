d61f9dfc70c4eb156435d1e533b2dac227b79189
r21nomi <ryotakosu@gmail.com>
2016-02-25 02:50:45 +0900

    Fix sample

--- sample/src/main/java/com/github/r21nomi/sample/androidrpinterpolator/MainActivity.java
+++ sample/src/main/java/com/github/r21nomi/sample/androidrpinterpolator/MainActivity.java
@@ -5,28 +5,47 @@ import android.content.Context;
 import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Display;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.github.r21nomi.androidrpinterpolator.RPInterpolator;
 import com.github.r21nomi.sample.R;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final long ANIMATE_DURATION = 1500;
+    private static final long ANIMATE_DURATION = 1000;
+    private Map<RPInterpolator.Easing, View> mAnimateTargetSets = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setInterpolatorName();
+        setAnimateTargetViews();
         startAnimation();
+    }
+
+    private void setAnimateTargetViews() {
+        for (RPInterpolator.Easing easing : RPInterpolator.Easing.values()) {
+            View view = getLayoutInflater().inflate(R.layout.item, null);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startAnimation();
+                }
+            });
+            ((TextView)view.findViewWithTag("name"))
+                    .setText(easing.toString());
+            ((LinearLayout)findViewById(R.id.root)).addView(view);
+
+            mAnimateTargetSets.put(easing, view);
+        }
     }
 
     private void startAnimation() {
@@ -36,85 +55,16 @@ public class MainActivity extends AppCompatActivity {
 
         float x = size.x - 600;
 
-        ObjectAnimator animator_1 = ObjectAnimator.ofFloat(findViewById(R.id.item1)
-                .findViewWithTag("target"), "translationX", 0, x);
-        animator_1.setDuration(ANIMATE_DURATION);
-        animator_1.setInterpolator(new RPInterpolator(RPInterpolator.Easing.SINE_IN_OUT));
-        animator_1.start();
-
-        ObjectAnimator animator_2 = ObjectAnimator.ofFloat(findViewById(R.id.item2)
-                .findViewWithTag("target"), "translationX", 0, x);
-        animator_2.setDuration(ANIMATE_DURATION);
-        animator_2.setInterpolator(new RPInterpolator(RPInterpolator.Easing.QUAD_IN_OUT));
-        animator_2.start();
-
-        ObjectAnimator animator_3 = ObjectAnimator.ofFloat(findViewById(R.id.item3)
-                .findViewWithTag("target"), "translationX", 0, x);
-        animator_3.setDuration(ANIMATE_DURATION);
-        animator_3.setInterpolator(new RPInterpolator(RPInterpolator.Easing.CUBIC_IN_OUT));
-        animator_3.start();
-
-        ObjectAnimator animator_4 = ObjectAnimator.ofFloat(findViewById(R.id.item4)
-                .findViewWithTag("target"), "translationX", 0, x);
-        animator_4.setDuration(ANIMATE_DURATION);
-        animator_4.setInterpolator(new RPInterpolator(RPInterpolator.Easing.QUART_IN_OUT));
-        animator_4.start();
-
-        ObjectAnimator animator_5 = ObjectAnimator.ofFloat(findViewById(R.id.item5)
-                .findViewWithTag("target"), "translationX", 0, x);
-        animator_5.setDuration(ANIMATE_DURATION);
-        animator_5.setInterpolator(new RPInterpolator(RPInterpolator.Easing.QUINT_IN_OUT));
-        animator_5.start();
-
-        ObjectAnimator animator_6 = ObjectAnimator.ofFloat(findViewById(R.id.item6)
-                .findViewWithTag("target"), "translationX", 0, x);
-        animator_6.setDuration(ANIMATE_DURATION);
-        animator_6.setInterpolator(new RPInterpolator(RPInterpolator.Easing.EXPO_IN_OUT));
-        animator_6.start();
-
-        ObjectAnimator animator_7 = ObjectAnimator.ofFloat(findViewById(R.id.item7)
-                .findViewWithTag("target"), "translationX", 0, x);
-        animator_7.setDuration(ANIMATE_DURATION);
-        animator_7.setInterpolator(new RPInterpolator(RPInterpolator.Easing.CIRC_IN_OUT));
-        animator_7.start();
-
-        ObjectAnimator animator_8 = ObjectAnimator.ofFloat(findViewById(R.id.item8)
-                .findViewWithTag("target"), "translationX", 0, x);
-        animator_8.setDuration(ANIMATE_DURATION);
-        animator_8.setInterpolator(new RPInterpolator(RPInterpolator.Easing.BACK_IN_OUT));
-        animator_8.start();
-
-        ObjectAnimator animator_9 = ObjectAnimator.ofFloat(findViewById(R.id.item9)
-                .findViewWithTag("target"), "translationX", 0, x);
-        animator_9.setDuration(ANIMATE_DURATION);
-        animator_9.setInterpolator(new RPInterpolator(RPInterpolator.Easing.ELASTIC_IN_OUT));
-        animator_9.start();
-
-        ObjectAnimator animator_10 = ObjectAnimator.ofFloat(findViewById(R.id.item10)
-                .findViewWithTag("target"), "translationX", 0, x);
-        animator_10.setDuration(ANIMATE_DURATION);
-        animator_10.setInterpolator(new RPInterpolator(RPInterpolator.Easing.BOUNCE_IN_OUT));
-        animator_10.start();
-    }
-
-    private void setInterpolatorName() {
-        findTextViewWithTag(findViewById(R.id.item1), "name").setText(RPInterpolator.Easing.SINE_IN_OUT.toString());
-        findTextViewWithTag(findViewById(R.id.item2), "name").setText(RPInterpolator.Easing.QUAD_IN_OUT.toString());
-        findTextViewWithTag(findViewById(R.id.item3), "name").setText(RPInterpolator.Easing.CUBIC_IN_OUT.toString());
-        findTextViewWithTag(findViewById(R.id.item4), "name").setText(RPInterpolator.Easing.QUART_IN_OUT.toString());
-        findTextViewWithTag(findViewById(R.id.item5), "name").setText(RPInterpolator.Easing.QUINT_IN_OUT.toString());
-        findTextViewWithTag(findViewById(R.id.item6), "name").setText(RPInterpolator.Easing.EXPO_IN_OUT.toString());
-        findTextViewWithTag(findViewById(R.id.item7), "name").setText(RPInterpolator.Easing.CIRC_IN_OUT.toString());
-        findTextViewWithTag(findViewById(R.id.item8), "name").setText(RPInterpolator.Easing.BACK_IN_OUT.toString());
-        findTextViewWithTag(findViewById(R.id.item9), "name").setText(RPInterpolator.Easing.ELASTIC_IN_OUT.toString());
-        findTextViewWithTag(findViewById(R.id.item10), "name").setText(RPInterpolator.Easing.BOUNCE_IN_OUT.toString());
+        for (RPInterpolator.Easing easing : mAnimateTargetSets.keySet()) {
+            ObjectAnimator animator = ObjectAnimator.ofFloat(
+                    mAnimateTargetSets.get(easing).findViewWithTag("target"), "translationX", 0, x);
+            animator.setDuration(ANIMATE_DURATION);
+            animator.setInterpolator(new RPInterpolator(easing));
+            animator.start();
+        }
     }
 
     private Display getDisplay(final Context context) {
         return ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-    }
-
-    private TextView findTextViewWithTag(View container, String tag) {
-        return (TextView)container.findViewWithTag(tag);
     }
 }
