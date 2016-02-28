2b220682b649265e3032a9e0a00cbe3a270e5148
r21nomi <ryotakosu@gmail.com>
2016-02-28 23:42:15 +0900

    Refactor and :arrow_up: update version to 1.0.0

---
+++ library/src/main/java/com/github/r21nomi/androidrpinterpolator/Easing.java
@@ -0,0 +1,38 @@
+package com.github.r21nomi.androidrpinterpolator;
+
+/**
+ * Created by Ryota Niinomi on 16/02/28.
+ */
+public enum Easing {
+    LINEAR,
+    SINE_IN,
+    SINE_OUT,
+    SINE_IN_OUT,
+    QUAD_IN,
+    QUAD_OUT,
+    QUAD_IN_OUT,
+    CUBIC_IN,
+    CUBIC_OUT,
+    CUBIC_IN_OUT,
+    QUART_IN,
+    QUART_OUT,
+    QUART_IN_OUT,
+    QUINT_IN,
+    QUINT_OUT,
+    QUINT_IN_OUT,
+    EXPO_IN,
+    EXPO_OUT,
+    EXPO_IN_OUT,
+    CIRC_IN,
+    CIRC_OUT,
+    CIRC_IN_OUT,
+    BACK_IN,
+    BACK_OUT,
+    BACK_IN_OUT,
+    ELASTIC_IN,
+    ELASTIC_OUT,
+    ELASTIC_IN_OUT,
+    BOUNCE_IN,
+    BOUNCE_OUT,
+    BOUNCE_IN_OUT
+}
--- library/src/main/java/com/github/r21nomi/androidrpinterpolator/RPInterpolator.java
+++ library/src/main/java/com/github/r21nomi/androidrpinterpolator/RPInterpolator.java
@@ -12,40 +12,6 @@ public class RPInterpolator implements Interpolator {
 
     private Easing mEasing;
 
-    public enum Easing {
-        LINEAR,
-        SINE_IN,
-        SINE_OUT,
-        SINE_IN_OUT,
-        QUAD_IN,
-        QUAD_OUT,
-        QUAD_IN_OUT,
-        CUBIC_IN,
-        CUBIC_OUT,
-        CUBIC_IN_OUT,
-        QUART_IN,
-        QUART_OUT,
-        QUART_IN_OUT,
-        QUINT_IN,
-        QUINT_OUT,
-        QUINT_IN_OUT,
-        EXPO_IN,
-        EXPO_OUT,
-        EXPO_IN_OUT,
-        CIRC_IN,
-        CIRC_OUT,
-        CIRC_IN_OUT,
-        BACK_IN,
-        BACK_OUT,
-        BACK_IN_OUT,
-        ELASTIC_IN,
-        ELASTIC_OUT,
-        ELASTIC_IN_OUT,
-        BOUNCE_IN,
-        BOUNCE_OUT,
-        BOUNCE_IN_OUT
-    }
-
     public RPInterpolator() {
         mEasing = Easing.LINEAR;
     }
